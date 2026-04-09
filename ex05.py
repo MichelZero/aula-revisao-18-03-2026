bbf9c6aa497beee940b9b0295611dde9891347d6
MichelZero <silva.michel@ifpb.edu.br>
2026-04-09 09:44:35 -0300

    up

---
+++ ex05.py
@@ -0,0 +1,8 @@
+# frasco Atual
+# V = pi*(r)^2*h
+pi = 3.1415
+r = int(input("informe o Raio: "))
+h = float(input("inf. a altura: "))
+
+v = pi*(r**2)*h
+print (f"frasco = {v:.43f}")
---
+++ ex06.py
@@ -0,0 +1,6 @@
+contador = 3
+while contador > 0:
+    #print(contador)
+    print(f"contador = {contador}")
+    #contador = contador - 1 # ou contador -= 1
+print("Fim!")
